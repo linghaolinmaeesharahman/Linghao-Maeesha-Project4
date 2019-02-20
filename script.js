441c2b1f219d5e5aec0facb4ba8c809be6a610ed
Maeesha <maeesha.rahman@gmail.com>
2019-02-20 17:42:37 -0500

    attempting to get api info

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function(){
-    
+const makeupApp = {};
+
+makeupApp.getProducts = function(){
+    $.ajax({
+        url: 'http://makeup-api.herokuapp.com/api/v1/products.json',
+        method: 'GET',
+        dataType: 'json',
+    }).then(function (data) {
+        makeupApp.displayProducts(data.product_type);
+        console.log(makeupApp.displayProducts);
+    });
 }
