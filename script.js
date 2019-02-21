b1b47d657e0468e8015fa83d2bc5f070943044a6
Linghao Lin <linlinghao@gmail.com>
2019-02-20 23:05:22 -0500

    add event listener to first form submit button, and call API with the data collected from previous event listener

--- script.js
+++ script.js
@@ -1,36 +1,86 @@
+// step1: declare empty objects to store event ajax call, event handler
+// ajax call
 const makeupApp = {};
 
-const productType = ['blush', 'lipstick', ]
+//event handler
+const eventHandler = {};
+
+// step2: submit the first form to take user's input
+
+eventHandler.submitProductFilter = () => {
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        //find the product type
+        let $checkBoxInputs = $('input[name = product-type]:checked');
+        let productTypes = $checkBoxInputs.map(number => $checkBoxInputs[number].value);
+        //find the product price
+        let $productPrice = $('input[name = product-price]:checked').val();
+
+        // step4: call API with each product type as query string
+        for (let i = 0; i < productTypes.length; i++) {
+            makeupApp.getColors(productTypes[i], $productPrice)
+                //resolve the promise, get all the hex colors
+                .then((colors) => {
+                    //? randomize the color
+                    //?print the color on page
+                    console.log(colors)
+                });
+        }
+
+    });
+}
+
+// step3: take user's input and pass into API to filter the data we want.
+
 makeupApp.url = 'https://makeup-api.herokuapp.com/api/v1/products.json'
-makeupApp.getProducts = function(i){
-    $.ajax({
+makeupApp.getColors = function(type, price) {
+    //filter user's choice of price
+    let priceFilter = null;
+    if (price == '$') {
+        priceFilter = {
+            price_less_than: 10
+        };
+    } else {
+        priceFilter = {
+            price_greater_than: 10
+        };
+    }
+
+    //define the query string of URL
+    return $.ajax({
         url: makeupApp.url,
         method: 'GET',
         dataType: 'json',
         data: {
-            // q: product_type = productType[i]
-            product_type: productType[0],
+            product_type: type,
+            ...priceFilter
         }
-        
-    }).then(function (data) {
-        // makeupApp.displayProducts(data.product_type);
-        console.log(data);
-        // make loop to get product types 
-        console.log(data[0].product_type);
-        console.log(makeupApp.url);
+        //take the hex color of related data out and store them into an array
+    }).then(function(products) {
+        let allColors = [];
+        for (let i in products) {
+            let colorCollection = products[i].product_colors;
+            colorCollection.map((color) => allColors.push(color["hex_value"]));
+        }
+        return allColors;
     });
 }
 
-makeupApp.displayProducts = function(data) {
-    console.log(data);
-};
+// step5: add eventlistener to 'give me another one' button
+eventHandler.newPaletteGenerator = () => {
+    $('button[name="palette"]').on('click'.function(event) {
+        event.preventDefault();
+    })
+}
 
-// makeupApp.getProducts();
-makeupApp.init = function () {
-    makeupApp.getProducts(1);
-    console.log(makeupApp.getProducts);
-};
+// step:6 add eventlistener to palette radio, to collect user's input of color
+// print to SVG
 
-$(function () {
-    makeupApp.init();
+
+// step:7 add eventlistener to confirm button, to collect user's choice of color
+
+$(function() {
+    eventHandler.submitProductFilter();
+
+    // makeupApp.getColors('blush', '$'); // => ['#123', ...]
 });
